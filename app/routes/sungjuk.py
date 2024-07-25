c60b237b49660902e39b9b678afeaa06858fe2eb
pq100 <system32bit@naver.com>
2024-07-25 14:17:52 +0900

    /sungjuk 요청에 대한 뷰 추가

---
+++ app/routes/sungjuk.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, Request
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
+
+# 라우터 생성
+sungjuk_router = APIRouter()
+# 템플릿 지정
+templates = Jinja2Templates(directory="views/templates")
+
+# 라우트 설정
+@sungjuk_router.get('/', response_class=HTMLResponse)
+async def sungjuk(req: Request):
+    return  templates.TemplateResponse('sungjuk/sungjuk.html', {'request': req})
+
+@sungjuk_router.post('/', response_class=HTMLResponse)
+async def sungjukok():
+    pass
