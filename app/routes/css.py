6ed42cde920efe4638b5f8e2d71dc02bee71c49f
pq100 <system32bit@naver.com>
2024-07-31 18:15:25 +0900

    css3 - list

--- app/routes/css.py
+++ app/routes/css.py
@@ -29,3 +29,7 @@ async def text(req: Request):
 @css_router.get('/box')
 async def box(req: Request):
     return templates.TemplateResponse('css/04box.html', {'request': req})
+
+@css_router.get('/list')
+async def lists(req: Request):
+    return templates.TemplateResponse('css/05list.html', {'request': req})
