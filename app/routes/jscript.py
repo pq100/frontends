d2b6135a54fd857628adaf89285b3d75edb664bc
pq100 <system32bit@naver.com>
2024-08-12 14:55:44 +0900

    우편번호 검색 api 추가 수정 (jscript)

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -1,84 +1,92 @@
 import sqlite3
 
-from fastapi import APIRouter, Request
+from fastapi import APIRouter
+from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.templating import Jinja2Templates
 
 # 라우터 생성
 jscript_router = APIRouter()
-
 # 템플릿 지정
 templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
-
 @jscript_router.get('/')
-async def semantic(request: Request):
-    return templates.TemplateResponse('js/index.html', {'request': request})
+async def index(req: Request):
+    return templates.TemplateResponse('js/index.html', {'request': req})
 
 @jscript_router.get('/hello')
-async def hello(request: Request):
-    return templates.TemplateResponse('js/01hello.html', {'request': request})
+async def hello(req: Request):
+    return templates.TemplateResponse('js/01hello.html', {'request': req})
 
 @jscript_router.get('/type')
-async def type(request: Request):
-    return templates.TemplateResponse('js/02type.html', {'request': request})
+async def type(req: Request):
+    return templates.TemplateResponse('js/02type.html', {'request': req})
 
 @jscript_router.get('/operator')
-async def operator(request: Request):
-    return templates.TemplateResponse('js/03operator.html', {'request': request})
+async def flex(req: Request):
+    return templates.TemplateResponse('js/03operator.html', {'request': req})
 
 @jscript_router.get('/condition')
-async def condition(request: Request):
-    return templates.TemplateResponse('js/04condition.html', {'request': request})
+async def condition(req: Request):
+    return templates.TemplateResponse('js/04condition.html', {'request': req})
 
 @jscript_router.get('/loop')
-async def loop(request: Request):
-    return templates.TemplateResponse('js/05loop.html', {'request': request})
+async def loop(req: Request):
+    return templates.TemplateResponse('js/05loop.html', {'request': req})
 
 @jscript_router.get('/array')
-async def array(request: Request):
-    return templates.TemplateResponse('js/06array.html', {'request': request})
+async def array(req: Request):
+    return templates.TemplateResponse('js/06array.html', {'request': req})
 
 @jscript_router.get('/while')
-async def while1(request: Request):
-    return templates.TemplateResponse('js/07while.html', {'request': request})
+async def whiles(req: Request):
+    return templates.TemplateResponse('js/07while.html', {'request': req})
 
 @jscript_router.get('/function')
-async def function(request: Request):
-    return templates.TemplateResponse('js/08function.html', {'request': request})
+async def function(req: Request):
+    return templates.TemplateResponse('js/08function.html', {'request': req})
 
 @jscript_router.get('/callback')
-async def callback(request: Request):
-    return templates.TemplateResponse('js/09callback.html', {'request': request})
+async def callback(req: Request):
+    return templates.TemplateResponse('js/09callback.html', {'request': req})
 
 @jscript_router.get('/except')
-async def except1(request: Request):
-    return templates.TemplateResponse('js/10except.html', {'request': request})
+async def excepts(req: Request):
+    return templates.TemplateResponse('js/10except.html', {'request': req})
 
 @jscript_router.get('/bom')
-async def bom(request: Request):
-    return templates.TemplateResponse('js/11bom.html', {'request': request})
+async def bom(req: Request):
+    return templates.TemplateResponse('js/11bom.html', {'request': req})
 
 @jscript_router.get('/dom')
-async def dom(request: Request):
-    return templates.TemplateResponse('js/12dom.html', {'request': request})
+async def dom(req: Request):
+    return templates.TemplateResponse('js/12dom.html', {'request': req})
+
+@jscript_router.get('/event')
+async def event(req: Request):
+    return templates.TemplateResponse('js/13event.html', {'request': req})
+
+@jscript_router.get('/form')
+async def form(req: Request):
+    return templates.TemplateResponse('js/14form.html', {'request': req})
+
+@jscript_router.get('/ajax')
+async def ajax(req: Request):
+    return templates.TemplateResponse('js/15ajax.html', {'request': req})
 
 @jscript_router.get('/zipcode')
-async def zipcode(request: Request):
-    return templates.TemplateResponse('js/16zipcode.html', {'request': request})
+async def zipcode(req: Request):
+    return templates.TemplateResponse('js/16zipcode.html', {'request': req})
 
 @jscript_router.get('/ajaxzip')
-async def ajaxzip(request: Request):
-    return templates.TemplateResponse('js/17ajaxzip.html', {'request': request})
+async def ajaxzip(req: Request):
+    return templates.TemplateResponse('js/17ajaxzip.html', {'request': req})
 
-@jscript_router.get('/zip2013')
-async def zip2013(request: Request):
-    return templates.TemplateResponse('js/17ajaxzip.html', {'request': request})
-
-@jscript_router.get('/getsido')
 # http://127.0.0.1:8000/js/getsido
-async def getsido(request: Request):
+@jscript_router.get('/getsido')
+async def getsido():
+    # zipcode테이블에서 시도 조회
     conn = sqlite3.connect('app/schema/python.db')
     cursor = conn.cursor()
     sql = 'select distinct sido from zipcode2013'
@@ -93,10 +101,10 @@ async def getsido(request: Request):
 
     return JSONResponse(content=result)
 
-@jscript_router.get('/getgugun')
 # http://127.0.0.1:8000/js/getgugun?sido=서울
+@jscript_router.get('/getgugun')
 async def getgugun(sido: str):
-    # zipcode 테이블에서 구군 조회
+    # zipcode테이블에서 구군 조회
     conn = sqlite3.connect('app/schema/python.db')
     cursor = conn.cursor()
     sql = 'select distinct gugun from zipcode2013 where sido = ?'
@@ -111,15 +119,14 @@ async def getgugun(sido: str):
 
     return JSONResponse(content=result)
 
-
+# http://127.0.0.1:8000/js/getgugun?sido=서울&gugun=강남구
 @jscript_router.get('/getdong')
-# http://127.0.0.1:8000/js/getdong?sido=서울&gugu=강남구
-async def getdong(sido: str, gugu: str):
-    # zipcode 테이블에서 읍면동 조회
+async def getdong(sido: str, gugun: str):
+    # zipcode테이블에서 읍면동 조회
     conn = sqlite3.connect('app/schema/python.db')
     cursor = conn.cursor()
     sql = 'select distinct dong from zipcode2013 where sido = ? and gugun = ?'
-    cursor.execute(sql, (sido, gugu, ))
+    cursor.execute(sql, (sido, gugun,))
     dongs = cursor.fetchall()
     cursor.close()
     conn.close()
